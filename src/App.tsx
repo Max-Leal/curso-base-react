9ca769e71f4c932decf26ec50769e75556ba7990
Max-leal <Max13leal@gmail.com>
2026-02-01 14:05:04 -0300

    feat: basic component props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 
+interface ICardProps {
+  title: string;
+  children: React.ReactNode;
+}
+const Card = (props: ICardProps) => {
+
+  return (
+    <div style={{ border: '1px solid black'}}>
+      <span>Title: {props.title}</span>
+
+      <div>
+        {props.children}
+      </div>
+
+      <div>Footer</div>
+    </div>
+  )
+}
+
+
+
+
+
 export function App() {
 
-  return (<>
-  <p>Ola</p>
-  </>
+  return (
+    <div>
+      Ola
+      <p>Card:</p>
+      <Card title="teste">
+    Helo mano
+      </Card>
+    </div>
+
 
   )
 }
