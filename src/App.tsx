f07248f113ad58df01fdbe2e314591a91570d40b
Max-leal <Max13leal@gmail.com>
2026-02-01 17:22:11 -0300

    feat: adding new items to the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { useState } from 'react';
 
 export function App() {
-
+  const [value, setValue] = useState('')
   const [list, setList] = useState([
-    { id: 1, label: 'Fazer cafe',},
-    { id: 2, label: 'Fazer cafe',},
-    { id: 3, label: 'Fazer almoco',},
-    { id: 4, label: 'Fazer Janta',}
+    { id: '1', label: 'Fazer cafe', },
+    { id: '2', label: 'Fazer cafe', },
+    { id: '3', label: 'Fazer almoco', },
+    { id: '4', label: 'Fazer Janta', }
   ])
 
   return (
     <div>
 
-      <input />
-      <button>Adicionar</button>
+      <input value={value}
+        onChange={(e) => setValue(e.target.value)} />
+
+      <button onClick={() => {
+        setList([...list,
+        { id: (list.length + 1).toString(), label: value }
+        ]);
+        setValue('');
+      }
+      }>
+
+        Adicionar</button>
 
       <ol>
         {list.map(listItem => (
